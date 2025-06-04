c99bf00a34a3b46ee89541951c7594f477473516
Shubham Pingale <128289200+Pingale-Shubham@users.noreply.github.com>
2025-06-04 12:46:38 +0530

    Updates

---
+++ inc/send_email.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['send'])) {
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to      = "iamshubh2000@gmail.com"; // Replace with your email
+    $subject = "New Message from Website Contact Form";
+    $body    = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
